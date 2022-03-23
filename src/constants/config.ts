f56fb7267cb39c8112b8594e72183495a5687409
Paul Huggins <paulhuggins29@gmail.com>
2022-03-22 22:13:12 -0500

    qwerty update and titling

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -1,6 +1,6 @@
 export const CONFIG = {
   tries: 6, // This changes how many tries you get to finish the wordle
-  language: 'King James Version', // This changes the display name for your language
+  language: '', // This changes the display name for your language
   wordLength: 5, // This sets how long each word is based on how many characters (as defined in orthography.ts) are in each word
   author: 'Paul Huggins', // Put your name here so people know who made this Wordle!
   authorWebsite: 'phuggins.github.io', // Put a link to your website or social media here
